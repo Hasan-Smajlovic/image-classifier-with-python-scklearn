2785c392a192384a93d02e5dfbfe94c19461e2e4
Hasan Smajlovic <smajlovic.hasan02@gmail.com>
2026-07-07 14:27:04 +0200

    eat: add scikit-learn image classification pipeline

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+import numpy as np  
+
+import pickle
+
+from skimage.io import imread
+from skimage.transform import resize
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+
+
+# prepare data
+input_dir = 'C:\\Users\\smajl\\Downloads\\clf-data\\clf-data'
+categories = ['empty', 'non_empty']
+
+data = []
+labels = []
+
+for category_idx, category in enumerate(categories): 
+    for file in os.listdir(os.path.join(input_dir, category)):
+        img_path = os.path.join(input_dir, category, file)
+        img = imread(img_path)
+        img = resize(img, (15, 15))
+        data.append(img.flatten())
+        labels.append(category_idx)
+
+data = np.array(data)
+labels = np.array(labels)
+
+print(f"Data shape: {data.shape}")
+print(f"Labels shape: {labels.shape}")
+
+# train / test split
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size = 0.2, shuffle = True, stratify = labels, random_state = 42)
+
+# train classifier
+classifier = SVC()
+parameters = [{'gamma': [0.01, 0.001, 0.0001], 'C': [1, 10, 100, 1000]}]
+grid_search = GridSearchCV(classifier, parameters)
+grid_search.fit(X_train, y_train)
+
+
+# evaluate classifier
+best_estimator = grid_search.best_estimator_
+y_pred = best_estimator.predict(X_test)
+
+score = accuracy_score(y_test, y_pred)
+print(f"Best parameters: {grid_search.best_params_}")
+print(f"Accuracy: {score * 100:.2f}%")
+
+
+pickle.dump(best_estimator, open('./model.p', 'wb'))
